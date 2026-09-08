0738317d3e7990f9b72c2e90341024218e022914
davidsuson <susondavidimpo@gmail.com>
2026-09-08 18:57:13 +1000

    Fixed scrapbook.R being added to .gitignore

--- Workflows/scrapbook.R
+++ Workflows/scrapbook.R
@@ -1,2 +1,6 @@
-test <- download_nsw_datasets("C:/Users/David/Desktop/MicroEcon - Rent/RentAnalysis/Data/Raw Data/nsw")
-
+handle <- curl::new_handle(http_version = 1)
+curl::curl_download(
+  "https://www.dffh.vic.gov.au/quarterly-median-rents-local-government-area-september-quarter-2025-excel",
+  destfile = "C:/Users/David/Desktop/MicroEcon - Rent/RentAnalysis/Data/Raw Data/vic/VIC all quarters.xlsx",
+  mode = "wb"
+)
