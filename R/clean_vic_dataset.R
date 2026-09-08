af64acd6e5bed9783abf75303f4553eabb545f96
davidsuson <susondavidimpo@gmail.com>
2026-09-08 18:51:22 +1000

    Cleaned up pipe operator

--- R/clean_vic_dataset.R
+++ R/clean_vic_dataset.R
@@ -50,12 +50,12 @@ clean_vic_sheet <- function(raw_sheet, sheet_name) {
   # restructure data to required shape
 
   cols_to_pivot <- names(raw_sheet)[!names(raw_sheet) %in% c("  LGA", "  Region")]
-  pivoted_data <- raw_sheet %>%
+  pivoted_data <- raw_sheet |>
     tidyr::pivot_longer(
       cols = dplyr::all_of(cols_to_pivot),
       names_to = "Date",
       values_to = "Value"
-    ) %>%
+    ) |>
     tidyr::separate(Date,
                     into = c("Quarter", "Year", "Measure"),
                     sep = " ")
@@ -73,9 +73,9 @@ clean_vic_sheet <- function(raw_sheet, sheet_name) {
 
   dwelling <- stringr::str_trim(stringr::str_remove(sheet_name, "^\\d+br\\s*"))
 
-  data_with_additional_columns <- pivoted_data %>%
-    dplyr::mutate(dplyr::across(Value, ~ na_if(.x, "-"))) %>%
-    tidyr::pivot_wider(names_from = Measure, values_from = Value) %>%
+  data_with_additional_columns <- pivoted_data |>
+    dplyr::mutate(dplyr::across(Value, ~ na_if(.x, "-"))) |>
+    tidyr::pivot_wider(names_from = Measure, values_from = Value) |>
     dplyr::mutate(
       Quarter_Start_Date = dplyr::case_when(
         Quarter == "Mar" ~ as.Date(paste0(Year, "-01-01")),
@@ -92,13 +92,13 @@ clean_vic_sheet <- function(raw_sheet, sheet_name) {
       State = "VIC",
       Bedrooms = num_br,
       Dwelling = dwelling
-    ) %>%
+    ) |>
     dplyr::mutate(Bedrooms = as.character(Bedrooms))
   # This is because All Properties will have All Size in the bedroom field
 
   # clean up columns
 
-  data_with_cleaned_columns <- data_with_additional_columns %>%
+  data_with_cleaned_columns <- data_with_additional_columns |>
     dplyr::mutate(
       Quarter = dplyr::case_when(
         Quarter == "Mar" ~ 1,
@@ -106,15 +106,15 @@ clean_vic_sheet <- function(raw_sheet, sheet_name) {
         Quarter == "Sep" ~ 3,
         Quarter == "Dec" ~ 4
       )
-    ) %>%
-    dplyr::rename('New_Bonds' = "Count", 'Median_Rent' = "Median") %>%
+    ) |>
+    dplyr::rename('New_Bonds' = "Count", 'Median_Rent' = "Median") |>
     dplyr::mutate(
       New_Bonds = as.numeric(New_Bonds),
       Median_Rent = as.numeric(Median_Rent),
       Year = as.numeric(Year)
     )
 
-  final_data <- data_with_cleaned_columns %>%
+  final_data <- data_with_cleaned_columns |>
     dplyr::filter(Year >= 2018)
 
   return(final_data)
