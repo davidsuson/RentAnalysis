615f209f25f22f3682de562e0b3703247e8a77eb
davidsuson <susondavidimpo@gmail.com>
2026-09-16 21:48:00 +1000

    Untrack files now covered by .gitignore

--- Workflows/scrapbook.R
+++
@@ -1,6 +0,0 @@
-handle <- curl::new_handle(http_version = 1)
-curl::curl_download(
-  "https://www.dffh.vic.gov.au/quarterly-median-rents-local-government-area-september-quarter-2025-excel",
-  destfile = "C:/Users/David/Desktop/MicroEcon - Rent/RentAnalysis/Data/Raw Data/vic/VIC all quarters.xlsx",
-  mode = "wb"
-)
