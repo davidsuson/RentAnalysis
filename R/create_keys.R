733262cf89a762d061ba4b92a165fecee6f45dd3
davidsuson <susondavidimpo@gmail.com>
2026-09-18 21:40:13 +1000

    Finished mapping the income statistic table to the rent table by name

--- R/create_keys.R
+++ R/create_keys.R
@@ -3,50 +3,78 @@
 #' creates a mapping key for income earners,by geography Table 1.5 LGA names to LGA names in combined_dataset_no_controls.xlsx.
 #' This only maps names that are not identical. The key will be used to replace the values before a left join occurs.
 #'
-#' @returns A dataframe
+#' @returns A list of data-frames mapping missing LGA names from the rent dataset to the LGA names in the control tables
 #' @export
 create_keys <- function() {
 
-  missing_rent_data_income_names <- c(
+  income_LGA_name_key <- create_income_table_key()
+
+  keys <- list(
+    income = income_LGA_name_key,
+    demo = NULL
+  )
+
+  return(keys)
+}
+
+# HELPERS
+
+#' create_income_table_key
+#'
+#' Some LGA names in combined_dataset_no_controls.xlsx won't be present in income,earners by geography.xlsx.
+#' This just due to different names for the same LGA. This function lists the names in combined_dataset_no_controls.xlsx,
+#' that are not present in income,earners by geography.xlsx.
+#'
+#' It then lists, the equivalent names in income,earners by geography.xlsx in the vector matching_LGA_name_in_income_table
+#'
+#' @returns A tibble mapping the missing names in combined_dataset_no_controls.xlsx to their equivalent names in income,earners by geography.xlsx
+create_income_table_key <- function(){
+
+  rent_data_LGA_name_with_no_match <- c(
     "Ku-Ring-Gai",
     "Gundagai",
     "Western Plains Regional",
-    "Sydney City",
-    "Sutherland",
-    "The Hills",
-    "Bathurst",
-    "Glen Innes",
-    "Greater Hume",
-    "Tamworth",
-    "Upper Hunter",
-    "Upper Lachlan",
-    "Warrumbungle",
-    "Mornington Penin'a",
-    "Mallala"
-  )
-
-  matching_LGA_income_names <- c(
-    "Ku-ring-gai",
-    "Cootamundra-Gundagai Regional",
-    "Dubbo Regional",
-    "Sydney",
-    "Sutherland Shire",
-    "The Hills Shire",
-    "Bathurst Regional",
-    "Glen Innes Severn",
-    "Greater Hume Shire",
-    "Tamworth Regional",
-    "Upper Hunter Shire",
-    "Upper Lachlan Shire",
-    "Warrumbungle Shire",
-    "Mornington Peninsula",
     "Adelaide Plains"
   )
 
-  lga_name_mapping <- tibble::tibble(
-    missing_rent_data_income_names,
-    matching_LGA_demo_names
+  matching_LGA_name_in_income_table <- c(
+    "Ku-ring-gai",
+    "Cootamundra-Gundagai Regional",
+    "Dubbo Regional",
+    "Mallala"
   )
 
-  return(lga_demo_key)
+  income_LGA_name_key <- tibble::tibble(
+    rent_data_LGA_name_with_no_match,
+    matching_LGA_name_in_income_table
+  )
+
+  return(income_LGA_name_key)
+}
+
+#' create_demographic_table_key
+#'
+#' Some LGA names in combined_dataset_no_controls.xlsx won't be present in age and sex demographics.xlsx.
+#' This just due to different names for the same LGA. This function lists the names in combined_dataset_no_controls.xlsx,
+#' that are not present in age and sex demographics.xlsx.
+#'
+#' It then lists, the equivalent names in age and sex demographics.xlsx in the vector matching_LGA_name_in_demo_table
+#'
+#' @returns A tibble mapping the missing names in combined_dataset_no_controls.xlsx to their equivalent names in age and sex demographics.xlsx
+create_demographic_table_key <- function(){
+
+  rent_data_LGA_name_with_no_match <- c(
+    "Ku-Ring-Gai",
+  )
+
+  matching_LGA_name_in_demo_table <- c(
+    "Ku-ring-gai",
+  )
+
+  demo_LGA_name_key <- tibble::tibble(
+    rent_data_LGA_name_with_no_match,
+    matching_LGA_name_in_demo_table
+  )
+
+  return(demo_LGA_name_key)
 }
---
+++ R/join_controls.R
@@ -0,0 +1,48 @@
+#' join_controls
+#'
+#' @param data combined_dataset_no_controls.xlsx
+#' @param controls cleaned income and demographic data
+#' @seealso [clean_personal_income_statistics()], [clean_demographic_statistics()]
+#'
+#' @returns data with controls join by State, LGA and Year.
+#' @export
+join_controls <- function(data, controls){
+
+  missing_names_list <- lapply(controls, function(control) {
+    check_missing_LGA_names(data, control)
+  })
+
+  missing_names_list <- Filter(Negate(is.null), missing_names_list)
+
+  if (length(missing_names_list) > 0) {
+    stop(
+      "The following LGA names were not found in the control datasets and need to be manually mapped:\n",
+      paste(unique(unlist(missing_names_list)), collapse = ", "),
+      call. = FALSE
+    )
+  }
+
+  final_data_with_controls <- purrr::reduce(controls, dplyr::left_join, by = c("Year", "State", "LGA"), .init = data)
+
+  return(final_data_with_controls)
+}
+
+
+# HELPER
+
+#' check_missing_LGA_names
+#'
+#' @param data combined_dataset_no_controls.xlsx
+#' @param control one of the control datasets
+#'
+#' @returns NULL or vector of names that are not directly named in the control dataset. These names need to be mapped manually.
+check_missing_LGA_names <- function(data, control){
+
+  missing_names <- setdiff(data$LGA, control$LGA)
+
+  if (length(missing_names) == 0) {
+    return(NULL)
+  }
+
+  return(missing_names)
+}
