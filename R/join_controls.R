c5a7c8c9d7b6eb51882d1180446d3162275f0a56
davidsuson <susondavidimpo@gmail.com>
2026-09-18 22:16:04 +1000

    Finished joining and exporting final data with controls

--- R/join_controls.R
+++ R/join_controls.R
@@ -22,9 +22,7 @@ join_controls <- function(data, controls){
     )
   }
 
-  cols_to_left_join <- intersect(colnames(data), c("Year", "State", "LGA"))
-
-  final_data_with_controls <- purrr::reduce(controls, dplyr::left_join, by = cols_to_left_join, .init = data)
+  final_data_with_controls <- purrr::reduce(controls, dplyr::left_join, by = c("Year", "State", "LGA"), .init = data)
 
   return(final_data_with_controls)
 }
